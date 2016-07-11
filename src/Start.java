88378740b374b99bffee3a8529d157c214e5fdde
LCW <lingboling1991@126.com>
2016-07-11 20:13:23 +0800

    new branch created

--- src/Start.java
+++ src/Start.java
@@ -4,6 +4,6 @@
 public class Start {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("liu speaking");
+		System.out.println("dev speaking");
 	}
 }
